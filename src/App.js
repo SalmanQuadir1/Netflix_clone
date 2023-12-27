4d589fade03f1f703105145f3516b16aff22a07b
salman <najarsalman4@gmail.com>
2023-12-27 14:52:22 +0530

    created utils and configured tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 className='bg-red-600 text-white p-4 text-2xl'>
+        Hi ,Netflix is under construction !!!!
+
+      </h1>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/6b2842d1-2339-4f08-84f6-148e9fcbe01b/IN-en-20231218-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="" />
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
